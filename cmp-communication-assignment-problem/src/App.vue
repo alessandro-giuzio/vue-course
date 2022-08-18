b402b6715e429cd7532b2fc7f73b328e63028533
alessandro-giuzio <giuzio@icloud.com>
2022-08-18 12:46:18 +0200

    assignment-2

---
+++ cmp-communication-assignment-problem/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h1>Prova titolo</h1>
+    <active-user :username="user.name" :userage="user.age"></active-user>
+    <user-data @setdata="setUserData"></user-data>
+  </section>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: {
+        name: 'Alessandro Giuzio',
+        age: 48,
+      },
+    };
+  },
+  methods: {
+    setUserData(name, age) {
+      this.user = {
+        name: name,
+        age: age,
+      };
+    },
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  background: teal;
+}
+</style>
---
+++ cmp-communication-assignment-problem/src/components/ActiveUser.vue
@@ -0,0 +1,31 @@
+<template>
+  <section>
+    <h2>{{ username }}</h2>
+    <h3>{{ userage }} years</h3>
+  </section>
+</template>
+
+<script>
+export default {
+  props: {
+    username: {
+      type: String,
+      required: true,
+    },
+    userage: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style>
+section {
+  margin: 2rem auto;
+  padding: 4rem 2rem;
+  border: 2px solid #ccc;
+  max-width: 30rem;
+  border-radius: 12px;
+}
+</style>
